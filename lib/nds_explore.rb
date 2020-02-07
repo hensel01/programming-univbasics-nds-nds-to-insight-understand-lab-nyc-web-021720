26ca1667756b7ce255cb483922f71c09e753c0bb
Eliot Henson <eliotrhenson@gmail.com>
2020-02-07 17:23:31 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -9,4 +9,17 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
+index_row_one = 0 
+
+while index_row_one <=0  
+element_index_one =0 
+while element_index_one < directors_database[index_row_one][:movies].count    
+puts directors_database[index_row_one][:movies][element_index_one][:title]
+element_index_one +=1 
+end
+index_row_one+=2 
+end 
+
+
+end	
 end
